580302a98663d215d7e185b1d61704aa8ea586af
Claudia Ramos <cramos@fi.uba.ar>
2023-06-09 21:57:06 -0300

    Cambios en las clases del paquete controlador

--- ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/AutorControlador.java
+++ ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/AutorControlador.java
@@ -1,4 +1,17 @@
 package com.example.ProyectoFinal.controlador;
 
+import com.example.ProyectoFinal.servicio.AutorServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AutorControlador {
+
+    @Autowired
+    private AutorServicio autorServicio;
+
+
+
+
 }
--- ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/LibroControlador.java
+++ ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/LibroControlador.java
@@ -1,4 +1,41 @@
 package com.example.ProyectoFinal.controlador;
 
+import com.example.ProyectoFinal.modelo.Libro;
+import com.example.ProyectoFinal.servicio.LibroServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LibroControlador {
+
+    @Autowired
+    private LibroServicio libroServicio;
+
+    //@GetMapping("/login")
+    //public String iniciarSesion(){
+     //   return "login";
+    //}
+
+    @GetMapping("/")
+    public String verPaginaInicio(Model model) {
+        model.addAttribute("libro",libroServicio.listarTodasLosLibro());
+        //model.addAttribute("usuarios",usuarioServicio.listarTodos());
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/new/libro")
+    public String agregar(Model model) {
+        Libro libro = new Libro();
+
+        model.addAttribute("libro",libro);
+        return "crear_libro";
+
+    }
 }
