fe60369cc80c43289d5f83c4381abbabfc4d096e
Claudia Ramos <cramos@fi.uba.ar>
2023-06-12 21:23:59 -0300

    Se agregaron herramientas para eliminar el autor y actualizar el autor

--- ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/AutorControlador.java
+++ ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/AutorControlador.java
@@ -1,15 +1,82 @@
 package com.example.ProyectoFinal.controlador;
 
+import com.example.ProyectoFinal.modelo.Autor;
 import com.example.ProyectoFinal.servicio.AutorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class AutorControlador {
 
     @Autowired
     private AutorServicio autorServicio;
+
+    @GetMapping("/")
+    public String verPaginaDeInicio(Model model) {
+        model.addAttribute("autores",autorServicio.listarTodasLosAutores());
+        return "listarAutor";
+    }
+
+    @GetMapping("/nuevo/autor")
+    public String mostrarFormularioDeRegistroAutor(Model model) {
+        model.addAttribute("autor", new Autor());
+        return "crear_autor";
+    }
+
+    @PostMapping("/guardar/autor")
+    public String guardarContacto(@Validated Autor autor, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("autor",autor);
+            return "crear_autor";
+        }
+        autorServicio.guardarAutor(autor);
+        redirectAttributes.addFlashAttribute("msgExito","El autor ha sido agregado con exito");
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/autor/{id}")
+    public String mostrarFormularioDeEditarAutor(@PathVariable Integer id,Model model) {
+        Autor autor = autorServicio.obtenerPorId(id);
+        model.addAttribute("autor",autor);
+        return "editar_autor";
+    }
+
+    @PostMapping("/editar/autor/{id}")
+    public String actualizarAutor(@PathVariable Integer id, @Validated Autor autor, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        Autor autorDB = autorServicio.obtenerPorId(id);
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("autor",autor);
+            return "editar_autor";
+        }
+        autorDB.setId(id);
+        autorDB.setNombre(autor.getNombre());
+        autorDB.setApellido(autor.getApellido());
+        autorDB.setFechaNacimiento(autor.getFechaNacimiento());
+        autorDB.setLugarNacimiento(autor.getLugarNacimiento());
+
+        autorServicio.actualizarAutor(autorDB);
+
+        redirectAttributes.addFlashAttribute("msgExito","El autor ha sido actualizado exitosamente");
+        return "redirect:/";
+    }
+
+    @PostMapping("/eliminar/autor/{id}")
+    public String eliminarAutor(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Autor autor = autorServicio.obtenerPorId(id);
+        autorServicio.eliminarAutor(autor);
+        redirectAttributes.addFlashAttribute("msgExito","El autor ha sido eliminado exitosamente");
+        return "redirect:/";
+    }
+
 
 
 
--- ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/LibroControlador.java
+++ ProyectoFinal/src/main/java/com/example/ProyectoFinal/controlador/LibroControlador.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LibroControlador {
@@ -18,11 +20,11 @@ public class LibroControlador {
      //   return "login";
     //}
 
-    @GetMapping("/")
+    @GetMapping("hola")
     public String verPaginaInicio(Model model) {
         model.addAttribute("libro",libroServicio.listarTodasLosLibro());
         //model.addAttribute("usuarios",usuarioServicio.listarTodos());
-        return "index";
+        return "listarLibros";
     }
 
     @GetMapping("/home")
@@ -37,5 +39,11 @@ public class LibroControlador {
         model.addAttribute("libro",libro);
         return "crear_libro";
 
+    }
+
+    @PostMapping("/save/libro")
+    public String guardar(@ModelAttribute("libro") Libro libro) {
+        libroServicio.guardarLibro(libro);
+        return "redirect:/";
     }
 }
